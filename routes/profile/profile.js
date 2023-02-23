7384a4902aa7b5625e711f6b7a741a46a0bd21d5
shivamanand-dev <shivam_anand@outlook.com>
2023-02-23 20:02:04 +0530

    get user middleware

---
+++ routes/profile/profile.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const getUser = require("../../middleware/getUser");
+const router = express.Router();
+const User = require("../../models/auth/User");
+
+router.put("/update", getUser, async (req, res) => {
+  try {
+    const { liveCountry, name, profileImageUrl } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { name, liveCountry, profileImageUrl },
+      { returnDocument: "after" }
+    );
+
+    return res.send(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Server error occur" });
+  }
+});
+
+module.exports = router;
