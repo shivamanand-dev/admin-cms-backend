20ea00e6e50435aeb8e223f272751c21b2bfad38
shivamanand-dev <shivam_anand@outlook.com>
2023-02-25 02:37:20 +0530

    static page data

---
+++ routes/staticPageData/staticPageData.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const getUser = require("../../middleware/getUser");
+const StaticWebPageData = require("../../models/staticWebPageData/StaticWebPageData");
+const router = express.Router();
+
+router.get("/", getUser, async (req, res) => {
+  try {
+    const webSiteData = await StaticWebPageData.find({ user: req.user.id });
+    res.status(200).send(webSiteData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+router.post("/saveData", getUser, async (req, res) => {
+  try {
+    let success = false;
+
+    const staticWebPageData = await StaticWebPageData.create({
+      homePage: req.body,
+      user: req.user.id,
+    });
+
+    success = true;
+    res.status(200).send({ success, staticWebPageData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
